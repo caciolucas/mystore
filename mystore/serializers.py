5bd37aeddcb48c2a29668dc22712fba1ee944d72
Cacio Lucas <cclucas060901@gmail.com>
2021-09-08 15:12:52 -0300

    Refactor user serializer using less fields

--- mystore/serializers.py
+++ mystore/serializers.py
@@ -20,4 +20,4 @@ class CartItemSerializer(ModelSerializer):
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
