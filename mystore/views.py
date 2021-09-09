2896e6c00655045bfb5ae29a35ca3e39c3a44439
Cacio Lucas <cclucas060901@gmail.com>
2021-09-09 10:54:20 -0300

    Improving <userid>/cart/ route

--- mystore/views.py
+++ mystore/views.py
@@ -38,28 +38,52 @@ class UserViewSet(ModelViewSet):
             status_code = status.HTTP_200_OK
 
         elif request.method == 'POST':
-            cart_item = CartItem(user=get_object_or_404(User, pk=request.data['user']), product=get_object_or_404(Product, pk=request.data['product']), quantity=request.data['quantity'])
+            cart_item = CartItem.objects.filter(user=get_object_or_404(User, pk=pk),
+                                                product=get_object_or_404(Product, pk=request.data['product']))
+            if cart_item.exists():
+                cart_item = cart_item.first()
+                cart_item.quantity += request.data.get('quantity') if request.data.get('quantity') else 1
+                cart_item.save()
+            else:
+                cart_item = CartItem(user=get_object_or_404(User, pk=pk),
+                                     product=get_object_or_404(Product, pk=request.data['product']),
+                                     quantity= request.data.get('quantity') if request.data.get('quantity') else 1)
             cart_item.save()
             response = CartItemSerializer(cart_item).data
             status_code = status.HTTP_200_OK
 
+        elif request.method == 'PUT':
+            cart_item = CartItem.objects.filter(user=get_object_or_404(User, pk=pk),
+                                                product=get_object_or_404(Product, pk=request.data['product']))
+            if not cart_item.exists():
+                response = 'This product is not in this user\'s cart'
+                status_code = status.HTTP_404_NOT_FOUND
+            else:
+                cart_item = cart_item.first()
+                cart_item.quantity = request.data.get('quantity') if request.data.get('quantity') else cart_item.quantity
+                cart_item.quantity = request.data.get('product') if request.data.get('product') else cart_item.product
+                cart_item.save()
+                response = CartItemSerializer(cart_item).data
+                status_code = status.HTTP_200_OK
+
         elif request.method == 'DELETE':
-            items = CartItem.objects.filter(user=request.data['user'])
+            items = CartItem.objects.filter(user=pk)
             if request.data.get('product'):
-                items = items.filter(request.data['product'])
+                items = items.filter(product=request.data['product'])
             items.delete()
             response = 'Cart items deleted'
             status_code = status.HTTP_204_NO_CONTENT
 
-
         return Response(response, status_code)
+
 
 class LoginView(ViewSet):
     '''A simple viewset to verify user credentials and if it exists return its information'''
+
     def create(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
-        
+
         user_exists = User.objects.filter(username=username).exists()
 
         if user_exists:
@@ -71,4 +95,3 @@ class LoginView(ViewSet):
                 return Response(status=status.HTTP_401_UNAUTHORIZED)
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
-            
