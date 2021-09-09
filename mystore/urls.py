052fefad5d76253f49ee39def68018f206e956ab
Cacio Lucas <cclucas060901@gmail.com>
2021-09-08 21:24:39 -0300

    Simple login view

--- mystore/urls.py
+++ mystore/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from mystore.views import ProductViewSet, CartItemViewSet, UserViewSet
+from mystore.views import LoginView, ProductViewSet, CartItemViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('cart-items', CartItemViewSet)
 router.register('customers', UserViewSet)
+router.register('login', LoginView, basename='login')
 
 urlpatterns = [
     path('', include(router.urls)),
--- mystore/views.py
+++ mystore/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 # Create your views here.
 from mystore.models import Product, CartItem
 from mystore.serializers import ProductSerializer, CartItemSerializer, UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework import status
 
 
 class ProductViewSet(ModelViewSet):
@@ -25,3 +27,22 @@ class UserViewSet(ModelViewSet):
     @action(methods=['get', 'post'], detail=True)
     def cart(self, request, pk):
         return Response(CartItemSerializer(CartItem.objects.filter(user=pk), many=True).data)
+
+class LoginView(ViewSet):
+    '''A simple viewset to verify user credentials and if it exists return its information'''
+    def create(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        
+        user_exists = User.objects.filter(username=username).exists()
+
+        if user_exists:
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                return Response(UserSerializer(user).data)
+            else:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+            
